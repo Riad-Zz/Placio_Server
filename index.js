4061d7cae05564f4b834a728afbd89c7ba273033
Riad <kazuha3242@gmail.com>
2025-11-09 13:53:52 +0600

    Connetion to Database and basic CR api created

--- index.js
+++ index.js
@@ -1,21 +1,72 @@
-const express = require('express') ;
-const cors = require('cors') ;
-require('dotenv').config()
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
-const app = express() ;
-const port = process.env.PORT || 3000 ;
+//----------------Initial Port and Connections-------------------------
+const app = express();
+const port = process.env.PORT || 3000;
 
 //-----------------MiddleWire-------------------------
-app.use(cors()) ;
-app.use(express.json()) ;
+app.use(cors());
+app.use(express.json());
 
 //-------------------Test Api------------------------
-app.get('/' , (req,res)=>{
-    res.send("Placio Server Api! ")
-})
+app.get("/", (req, res) => {
+  res.send("Placio Server Api! ");
+});
+
+const uri = process.env.MONGO_URI;
+
+//--------------Mongo Client---------------------------------
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 
+//------------------------Connection to Mongo DB---------------------------
+async function run() {
+  try {
+//---------------------- Reminder->Comment this Out when deploying to vercel----------------------
+    await client.connect();
+    
+//-------------------------Databases & all Collection here-----------------------------
+    const dataBase = client.db("Placio") ;
+    const placioProperties = dataBase.collection("properties") ;
 
-app.listen(port , ()=>{
-    console.log(`Sever is Running on port : ${port}`)
-})
+    //--------------------All api here ---------------------------------------
+
+    //-----------------------Simple Post api to Add property------------------------
+    app.post('/property', async(req,res)=>{
+        const newProperty = req.body ;
+        const result = await placioProperties.insertOne(newProperty) ;
+        res.send(result) ;
+    })
+
+    //----------------------Simple Api to get Property ------------------------
+    app.get('/property' , async(req,res)=>{
+        const cursor =await placioProperties.find() ;
+        const result =await cursor.toArray() ;
+        res.send(result) ;
+    })
+
+
+    //----------------------- Reminder  -> Comment this Out when deploying to vercel
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Sever is Running on port : ${port}`);
+});
